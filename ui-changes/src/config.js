1adfd91f4b02f4417f7e2d3a942735791be72bab
Darius Petermann <darius.petermann@gmail.com>
2021-01-31 18:26:38 -0700

    Removed banner from config

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,7 +1,6 @@
 module.exports = {
   /* eslint-disable */
   githubSrc: 'https://raw.githubusercontent.com/sensein/MumbleMelody_Long/master/MumbleMelody/MumbleMelody_schema',
-  banner: 'Study on Stuttering',
   startButton: 'Join',
   assetsPublicPath: '/MumbleMelody_Long/',
   backendServer: 'https://sig.mit.edu/vb',
